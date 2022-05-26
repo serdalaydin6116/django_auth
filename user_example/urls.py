ea031106c59c102cb662634243d8b3df4ee075b8
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-05-26 22:55:28 +0300

    settings.py was set

--- user_example/urls.py
+++ user_example/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('special', special, name="special"),
     path('register', register, name="register"),
     path('change_password/', auth_views.PasswordChangeView.as_view(template_name = 'registration/change_password.html'), name="change_password"),
-     path('reset_password/', auth_views.PasswordResetView.as_view(template_name = 'registration/reset_password.html'), name="reset_password"),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name = 'registration/reset_password.html'), name="reset_password"),
 ]
--- user_example/views.py
+++ user_example/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 def home_view(request):    
     return render(request, "user_example/home.html")
